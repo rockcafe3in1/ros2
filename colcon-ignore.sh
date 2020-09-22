b1faab1fb470f607b5015f2eac983b203a25a138
Ahmed Sobhy <asobhy@blackberry.com>
2020-09-22 14:15:47 -0400

    ignoring cyclonedds from build

--- colcon-ignore.sh
+++ colcon-ignore.sh
@@ -5,5 +5,7 @@ touch src/ros-visualization/COLCON_IGNORE
 touch src/ros2/rviz/COLCON_IGNORE
 echo 'uncrustify will be ignored'
 touch src/ament/uncrustify_vendor/COLCON_IGNORE
+echo 'CycloneDDS will be ignored'
+touch src/eclipse-cyclonedds/COLCON_IGNORE
 #echo 'foonathan memory is built with dependencies and will be ignored'
 #touch src/eProsima/foonathan_memory_vendor/COLCON_IGNORE
