c1b0e2ac69e1f3b671117e3bf63756989d5716c9
Ahmed Sobhy <asobhy@blackberry.com>
2020-09-22 13:04:27 -0400

    fix uncrustify path

--- colcon-ignore.sh
+++ colcon-ignore.sh
@@ -4,6 +4,6 @@ echo 'vistalization packages will be ignored'
 touch src/ros-visualization/COLCON_IGNORE
 touch src/ros2/rviz/COLCON_IGNORE
 echo 'uncrustify will be ignored'
-touch src/ament/uncrustify/COLCON_IGNORE
+touch src/ament/uncrustify_vendor/COLCON_IGNORE
 #echo 'foonathan memory is built with dependencies and will be ignored'
 #touch src/eProsima/foonathan_memory_vendor/COLCON_IGNORE
