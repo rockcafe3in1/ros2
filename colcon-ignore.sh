a6a3dedc884b78d9b8e7e0ddd6c17e46607632fd
Ahmed Sobhy <asobhy@blackberry.com>
2020-09-22 16:22:07 -0400

    ignore mimick package

--- colcon-ignore.sh
+++ colcon-ignore.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo 'Adding COLCON_IGNORE to packages that will not be built for QNX'
+
 echo 'vistalization packages will be ignored'
 touch src/ros-visualization/COLCON_IGNORE
 touch src/ros2/rviz/COLCON_IGNORE
+
 echo 'uncrustify will be ignored'
 touch src/ament/uncrustify_vendor/COLCON_IGNORE
+
 echo 'CycloneDDS will be ignored'
 touch src/eclipse-cyclonedds/COLCON_IGNORE
-#echo 'foonathan memory is built with dependencies and will be ignored'
-#touch src/eProsima/foonathan_memory_vendor/COLCON_IGNORE
+
+echo 'mimick will be ignored'
+touch src/ros2/mimick_vendor/COLCON_IGNORE
