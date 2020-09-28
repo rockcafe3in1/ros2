477d509cd5bc45ae7c5d89596722e9a4110c1945
Ahmed Sobhy <asobhy@blackberry.com>
2020-09-28 18:18:20 -0400

    trap exit codes

--- qnx_deps/build-deps.sh
+++ qnx_deps/build-deps.sh
@@ -2,6 +2,9 @@
 
 set e-
 
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
 echo "Building ROS2 QNX Dependencies..."
 
 start=$(date +%s.%N)
